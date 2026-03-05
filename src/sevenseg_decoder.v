6b838bac4688fb09db0bd41d9c62623767ca4688
pabloramirez16 <C28pablo.ramirez@afacademy.af.edu>
2026-03-04 21:36:49 -0700

    test

---
+++ src/sevenseg_decoder.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/04/2026 09:35:09 PM
+// Design Name: 
+// Module Name: sevenseg_decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenseg_decoder(
+    input [3:0] i_hex,
+    output [6:0] o_seg_n
+    );
+endmodule
